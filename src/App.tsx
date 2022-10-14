afabbb51b7a7cb69d08bcde4771d2a253bdf06e8
Dimkysy <diman.nesterov1995@gmail.com>
2022-10-14 17:00:23 +0300

    test merg repeat 1_2

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,7 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
+           <p>Helo</p>
             </div>
         </div>
     );
