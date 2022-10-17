d499825ee138c84cda9905a0e35def00c8af2e30
Dimkysy <diman.nesterov1995@gmail.com>
2022-10-17 19:29:21 +0300

    all zero repeat 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
-import { Todolist } from './Todolist';
+import {Todolist} from "./Todolist";
 
 function App() {
+
+    let task1 = [
+        {id:1, title:"React", isDone:false},
+        {id:2, title: "JS", isDone: true},
+        {id:3, title: "HTML & CSS", isDone: true},
+    ]
+
+    let task2 = [
+        {id:1, title:"Flex", isDone:false},
+        {id:2, title:"Grid", isDone: false},
+        {id:3, title: "Display & Inline", isDone: true},
+    ]
+
     return (
         <div className="App">
-           <Todolist/>
+            <Todolist  title="Hello" tasks ={task1}/>
+            <Todolist  title = "Bear" tasks={task2} />
         </div>
     );
 }
